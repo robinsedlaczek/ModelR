34dd19b5250d6c85fa4a8c420a730c2e9e5f641c
Robin Sedlaczek <robin.sedlaczek@live.de>
2014-10-14 00:56:30 +0200

    Add missing classes.

---
+++ WaveDev.ModelR.Shared/Models/SceneObjectInfoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WaveDev.ModelR.Shared.Models
+{
+    public class SceneObjectInfoModel
+    {
+        public SceneObjectInfoModel(Guid id, Guid sceneId)
+        {
+            Id = id;
+            SceneId = sceneId;
+        }
+
+        public Guid Id { get; set; }
+
+        public Guid SceneId { get; set; }
+
+        public SceneObjectType SceneObjectType { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ WaveDev.ModelR/Communication/ModelRHubClientProxy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace WaveDev.ModelR.Communication
+{
+    internal static class ModelRHubClientProxy
+    {
+        #region Private Fields
+
+        private static HubConnection s_connection;
+        private static IHubProxy s_proxy;
+
+        #endregion
+
+        #region Private Properties
+
+        private static HubConnection ModelRHubConnection
+        {
+            get
+            {
+                if (s_connection == null)
+                    InitSignalRConnection();
+
+                return s_connection;
+            }
+        }
+
+        private static IHubProxy ModelRHubProxy
+        {
+            get
+            {
+                if (s_proxy == null)
+                    InitSignalRProxy().Wait();
+
+                return s_proxy;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static async void InitSignalRConnection()
+        {
+            s_connection = new HubConnection("http://localhost:8082/")
+            {
+                TraceLevel = TraceLevels.All,
+                TraceWriter = Console.Out
+            };
+
+            await InitSignalRProxy();
+        }
+
+        private static async Task InitSignalRProxy()
+        {
+            s_proxy = ModelRHubConnection.CreateHubProxy("ModelRHub");
+
+            await ModelRHubConnection.Start();
+        }
+
+        #endregion
+    }
+}
