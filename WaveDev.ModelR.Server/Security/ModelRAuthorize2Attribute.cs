165415f1514561db68633eaeb444eda98fffeef4
Robin Sedlaczek <robin.sedlaczek@live.de>
2014-10-24 05:54:10 +0200

    Add authorization blocking AuthorizeAttribute for test issues.

---
+++ WaveDev.ModelR.Server/Security/ModelRAuthorize2Attribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WaveDev.ModelR.Server.Security
+{
+    public class ModelRAuthorize2Attribute : AuthorizeAttribute  
+    {
+        public override bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext hubIncomingInvokerContext, bool appliesToMethod)
+        {
+            return false;
+        }
+    }
+}
